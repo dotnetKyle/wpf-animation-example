decd1a49d71c7b948ada4a2866167ea6922b7356
Kyle <dotnetKyle@gmail.com>
2021-04-02 13:09:23 -0400

    Initial commit

---
+++ src/AnimationExample/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace AnimationExample
+{
+    /// <summary> Interaction logic for MainWindow.xaml </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+            => InitializeComponent();
+
+        private void OpenDialog_Click(object sender, RoutedEventArgs e)
+            => PopupDialog.ShowDialog(this);
+    }
+}
---
+++ src/AnimationExample/PopupDialog.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace AnimationExample
+{
+    /// <summary> Interaction logic for PopupDialog.xaml </summary>
+    public partial class PopupDialog : Window
+    {
+        public PopupDialog()
+            => InitializeComponent();
+        
+        private void Ok_Click(object sender, RoutedEventArgs e) 
+            => Close();
+
+        public static PopupDialog ShowDialog(Window owner)
+        {
+            var dlg = new PopupDialog();
+            dlg.Owner = owner;
+            dlg.ShowDialog();
+            return dlg;
+        }
+    }
+}
